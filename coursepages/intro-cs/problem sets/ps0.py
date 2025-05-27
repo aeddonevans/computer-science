15c00e7632fd6285a1c2bb89b2bda75cea6976bc
aeddonevans <evansaeddon@gmail.com>
2025-05-27 11:47:29 -0500

    Completed first problem set
    5/27/2025

---
+++ coursepages/intro-cs/problem sets/ps0.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# Problem Set 0
+# Name: Aeddon Evans
+# Time Spent: 1 minute
+
+import numpy
+
+x=5
+y=8
+
+z=x+y
+
+a=numpy.log2(z)
+
